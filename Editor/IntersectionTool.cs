4ce75eaabcaf9b91e5af1e3ae5b96937d9453614
vabyz971 <vabyz971@gmail.com>
2026-04-04 00:43:05 -0400

    interface adjustment

--- Editor/IntersectionTool.cs
+++ Editor/IntersectionTool.cs
@@ -6,44 +6,45 @@ namespace RedSnail.RoadTool.Editor;
 /// <summary>
 /// Create and manage road and road intersection.
 /// </summary>
-[Title("Create Road/Intersection")]
-[Icon("roundabout_left")]
-[Alias("intersection")]
-[Group("1")]
-[Order(0)]
+[Title( "Create Road/Intersection" )]
+[Icon( "roundabout_left" )]
+[Alias( "intersection" )]
+[Group( "1" )]
+[Order( 0 )]
 public class IntersectionTool : EditorTool
 {
-	public override Widget CreateToolSidebar()
-	{
-		var sidebar = new ToolSidebarWidget();
-
-		sidebar.MaximumSize = Vector2.One * 120.0f;
-
-		var group = sidebar.AddGroup("Create");
-
-		sidebar.CreateButton("Create Road", "route", null, CreateRoad, true, group);
-		sidebar.CreateButton("Create Intersection", "roundabout_left", null, CreateIntersection, true, group);
-
-		return sidebar;
-	}
-
-
-
 	public override void OnEnabled()
 	{
 
 	}
 
+	public override Widget CreateToolSidebar()
+	{
+		ToolSidebarWidget sidebar = new ToolSidebarWidget();
+		sidebar.AddTitle( "Intersection", "roundabout_left" );
 
+		Layout group = sidebar.AddGroup( "Create" );
+		Layout row = Layout.Row();
 
+		IconButton road = sidebar.CreateButton( "Create Road", "route", null, CreateRoad, true, row );
+		IconButton inter = sidebar.CreateButton( "Create Intersection", "roundabout_left", null, CreateIntersection, true, row );
+
+		row.Spacing = 5;
+		row.AddStretchCell();
+
+		group.Add( row );
+
+		sidebar.Layout.Add( group );
+		sidebar.Layout.AddStretchCell();
+		return sidebar;
+	}
+	
 	private static void CreateRoad()
 	{
 		GameObject go = SceneEditorSession.Active.Scene.CreateObject();
 		go.Name = "Road";
 		go.AddComponent<RoadComponent>();
 	}
-
-
 
 	private static void CreateIntersection()
 	{
